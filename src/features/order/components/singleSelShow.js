5f40fdba1a4a31d8d4e61ccc2ffdf2ee62230ea1
e <597395581@qq.com>
2020-04-09 10:42:07 +0800

    修改代码中‘==’和‘===’的用法，先前用的‘===’导致类型匹配不上的时候参数就去不到值，因此改为‘==’只判断值相等即可。

--- src/features/order/components/singleSelShow.js
+++ src/features/order/components/singleSelShow.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Descriptions } from 'antd'
 import _ from 'lodash'
 export default function(props) {
-  const show = _.find(props.params, o => o.value === props.default_value)
+  const show = _.find(props.params, o => o.value == props.default_value)
   return (
     <Descriptions.Item label={props.name} key={props.id}>
       <span style={{color: show.color ? show.color : null}}>{show.label}</span>
