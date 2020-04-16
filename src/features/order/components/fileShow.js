cbab81543b165dd9feabb0eb51e32c5143d283ca
e <597395581@qq.com>
2020-04-16 11:54:14 +0800

    现场环境的图片查看

--- src/features/order/components/fileShow.js
+++ src/features/order/components/fileShow.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Zmage from 'react-zmage'
 import './fileShow.less'
-          //{ <Zmage src={'https://fxtyyw.gaj.sh.gov.cn/wxPic?apikey=e10adc3949ba59abbe56e057f2gg88dd&file_id=' + f.id} /> }
 export default (props) => { 
   const {file} = props
   return (
     <div className="order-component-file">
       { file.map( f => 
         <div className="file-box" key={f.name}>
-          <Zmage src={'http://192.168.0.28/itsm/openapi/v2/tickets/downloadAttachFile?apikey=e10adc3949ba59abbe56e057f2gg88dd&file_id=' + f.id} />
+          <Zmage src={ <Zmage src={'https://fxtyyw.gaj.sh.gov.cn/wxPic?apikey=e10adc3949ba59abbe56e057f2gg88dd&file_id=' + f.id} /> } />
         </div>) }
     </div>
   )
