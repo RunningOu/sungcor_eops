09d685b4579959167197c9bcf51e71032c97e4a0
zmg <1643594984@qq.com>
2020-04-20 12:21:36 +0800

    报修时间问题

--- src/features/order/components/dateTimeShow.js
+++ src/features/order/components/dateTimeShow.js
@@ -5,7 +5,7 @@ const { Item } = Descriptions
 export default function(props) {
   return (
     <Item label={props.name} key={props.id}>
-      {/* {moment(props.default_value).format('YYYY-MM-DD HH:mm:ss')} */}
+        {moment(props.default_value).format('YYYY-MM-DD HH:mm:ss')} 
     </Item>
   )
 }
