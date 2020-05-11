6aeeb23c0d26700a02fc9d5879a6f31a96bfd924
xiaodie1 <lih@sungcor.net>
2020-05-11 16:46:47 +0800

    图像组管理员id

--- src/config.js
+++ src/config.js
@@ -6,3 +6,6 @@ export const USER_INFO_ID = 'sungcor_ds2d2_dnsadnjnfdjn_e10adc3949ba59abbe56e057
 
 // 图像组管理员id (公司)
 export const MANAGE_ID = '15f4c33d6b47463ba3c32d2b4aaefd0c'
+
+// 图像组管理员id (现场)
+// export const MANAGE_ID = '37dea9d684df4b3d947d677e12621611'
