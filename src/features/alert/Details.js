f690eee10ed352bf36f1c39fe468d353f624feba
xiaodie1 <lih@sungcor.net>
2020-05-12 13:44:45 +0800

    alert列表 scroll 加载更多数据

--- src/features/alert/Details.js
+++ src/features/alert/Details.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { HeaderBar, FooterBar } from '../common'
 // import { useHistory } from 'react-router-dom'
-import { Card, Tag, Row } from 'antd'
-// import { connect } from 'react-redux'
+import { Card, Tag, Row, message, List } from 'antd'
+import InfiniteScroll from 'react-infinite-scroller'
 import { formatDate } from '../../utils'
 import { queryAlertList } from '../../common/request'
 
@@ -13,41 +13,81 @@ const Details = (props) => {
   console.log(search)
   // const { userAccountInfo } = props
   // const history = useHistory()
-  const [alertNums, setAlertNums] = useState([])
+  const [alertList, setAlertList] = useState([])
+  const MESSAGE_KEY = 'messageKey'
   const [title, setTitle] = useState('')
+  const [count, setCount] = useState(0) // 列表总数
+  const [pageNum, setPageNum] = useState(1) // 列表分页下标
+  const [loading, setLoading] = useState(false) // 列表加载中状态
+  const [hasMore, setHasMore] = useState(true) // 列表加载中状态
+
+  const handleInfiniteOnLoad = () => {
+    setLoading(true)
+    if (alertList.length < 10) {
+      setHasMore(false)
+      setLoading(false)
+      return
+    }
+    setPageNum(current => current + 1)
+  }
+  useEffect(() => {
+    if (count > 0) {
+      message.success({ content: `共有${count}条告警记录`, key: MESSAGE_KEY})
+    }
+  }, [count])
   useEffect(() => {
     setTitle(new URLSearchParams(search).get('name')+"告警列表")
     const source = new URLSearchParams(search).get('name')
-    queryAlertList({ "source": source,'status':0 }).then(d => { setAlertNums(d.data.records) })
-  }, [search])
+    if(source.length) {
+      queryAlertList({
+        "source": source,
+        'status':0,
+        'pageNum': pageNum,
+        'pageSize': 10
+        }).then(d => { 
+          setCount(d.data.total)
+          // setAlertList(d.data.records)
+          if (d.data.hasOwnProperty('records')) {
+            if (d.data.records.length !== 10) setHasMore(false)
+            if(pageNum === 1) {
+              setAlertList([...d.data.records])
+            }else {
+              setAlertList((oldList) => [...oldList, ...d.data.records])
+            } 
+            setLoading(false)
+          } 
+      })
+      .catch((e) => { })
+    }
+  }, [search, pageNum])
   return (
     <div className="alert-details">
       <HeaderBar title={title} />
       <div className="alert-scoll">
-        {
-            alertNums.map((o, i) => (
-                <Row  className="alert-row" key={i}>
+      <InfiniteScroll
+          initialLoad={false}
+          pageStart={1}
+          loadMore={handleInfiniteOnLoad}
+          hasMore={!loading && hasMore}
+          useWindow={false}
+        >
+        <List dataSource={alertList} renderItem={item => (
+                // <Row  className="alert-row" key={i}>
                     <Card onClick={() => {
-                        console.log(o)
-                    }} title={<span>{o.source} 
+                        console.log(item)
+                    }} title={<span>{item.source} 
                     {/* <div className="arrow-left"></div> */}
-                    <Tag className={o.severityCN === '警告' ? 'alert-warning' : o.severityCN === '错误' ? 'alert-error' : 'alert-critical'}>{o.severityCN}</Tag ></span>} 
-                        extra={formatDate(new Date(o.lastOccurTime),'yyyy-MM-dd HH:mm')}
+                    <Tag className={item.severityCN === '警告' ? 'alert-warning' : item.severityCN === '错误' ? 'alert-error' : 'alert-critical'}>{item.severityCN}</Tag ></span>} 
+                        extra={formatDate(new Date(item.lastOccurTime),'yyyy-MM-dd HH:mm')}
                         headStyle={{'padding': '0px 0px 0px 5px', 'background': '#40a9ff', 'borderRadius': '10px', 'minHeight': '44px'}}
                         className="alert-card" key="alert_processed" bodyStyle={{ 'borderRadius': '10px', 'padding': '8px 2px 5px 14px' }}>
-                            <p>设备名称：<span>{o.entityName}</span></p>
-                            <p>设备IP：<span>{o.entityAddr}</span></p>
-                            <p>告警描述：<span>{o.description}</span></p>
-                            {/* <Col span={12}> */}
-                                {/* <p>首次发生时间：<span>{formatDate(new Date(o.firstOccurTime),'yyyy-MM-dd HH:mm:ss')}</span></p> */}
-                                {/* </Col> */}
-                            {/* <Col span={12}> */}
-                                {/* <p>最后发生时间：<span>{formatDate(new Date(o.lastOccurTime),'yyyy-MM-dd HH:mm:ss')}</span></p> */}
-                                {/* </Col> */}
+                            <p>设备名称：<span>{item.entityName}</span></p>
+                            <p>设备IP：<span>{item.entityAddr}</span></p>
+                            <p>告警描述：<span>{item.description}</span></p>
                     </Card>
-                </Row>
-            ))
-        }
+                // </Row>
+          )} />
+        </InfiniteScroll>
       </div>
       <FooterBar pathname={props.location.pathname} />
     </div>
