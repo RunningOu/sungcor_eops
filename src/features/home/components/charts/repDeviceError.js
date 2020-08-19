1da7fb4bb2f4bfa10e32179c43b6b0d6298939ea
xiaodie1 <lih@sungcor.net>
2020-08-19 14:51:09 +0800

    项目名称统计

--- src/features/home/components/charts/repDeviceError.js
+++ src/features/home/components/charts/repDeviceError.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import { Chart, Geom, Axis, Tooltip } from 'bizcharts'
-
-const data = [
-  { name: '监控设备', error: 25 },
-  { name: '视频设备', error: 15 },
-  { name: '办公设备', error: 17 },
-]
+import { Card, Statistic, Col, Radio } from 'antd'
+import { countByCode } from '../../../../common/request'
+// const data = [
+//   { name: '监控设备', error: 25 },
+//   { name: '视频设备', error: 15 },
+//   { name: '办公设备', error: 17 },
+// ]
 
 const cols = {
   error: { alias: '故障数量' },
   name: { alias: '设备种类'}
 }
-
+// var fxGzlxs = []
 export default () => {
+  const [cdata, setCdata] = useState([])
+  const [status, setStatus] = useState('week')
+  const statusR = ['week', 'month']
+  useEffect(() => {
+    // let errtype = [{ key: "status", value: "1,2", operator: "IN" }]
+    countByCode('fxGzlx', status).then(data => {
+      setCdata(data.data)
+    })
+  },[status])
   return (
-    <Chart height={250} data={data} scale={cols} forceFit padding="auto">
-      <Axis name="genre" title />
-      <Axis name="sold" title />
-      <Tooltip />
-      <Geom type="interval" position="name*error" color="name" />
-    </Chart>
+    <div style={{padding: '0px 0px 50px 0px'}}>
+      <Radio.Group value={status} onChange={
+        (e) => {
+          setStatus(e.target.value)
+        }
+      }>
+        <Radio.Button value={statusR[0]}>近七天</Radio.Button>
+        <Radio.Button value={statusR[1]}>近一个月</Radio.Button>
+        {/* <Radio.Button value="small">新告警</Radio.Button> */}
+      </Radio.Group>
+      <Chart height={250} data={cdata} scale={cols} forceFit padding="auto">
+        <Axis name="genre" title />
+        <Axis name="sold" title />
+        <Tooltip />
+        <Geom type="interval" position="name*error" />
+        {/* <Geom type="interval" position="name*error" color="name" /> */}
+      </Chart>
+    </div>
   )
 }
---
+++ src/features/home/components/charts/repTkXmmc.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect }  from 'react'
+import { Radio, Tag, Table } from 'antd'
+import { countTicketByStatus } from '../../../../common/request'
+import OrderModel from '../model/orderModel'
+export default () => {
+    const [cdata, setCdata] = useState([])
+    const [title, setTitle] = useState('')
+    const [status, setStatus] = useState('today')
+    const statusR = ['today', 'all']
+    const [visible, setVisible] = useState(false)
+    useEffect(() => {
+        countTicketByStatus(status).then(data => {
+          let sumTotal = 0
+          let sumUndone = 0
+          let sumOverdue = 0
+          data.result.forEach(element => {
+            sumTotal += element.total
+            sumUndone += element.undone
+            sumOverdue += element.overdue
+          })
+          setCdata([{'name': '总计', 'total': sumTotal, 'undone': sumUndone, 'overdue': sumOverdue}, ...data.result])
+          console.log(cdata)
+        })
+    },[status])
+    var columns = [
+        {
+            title: '项目名称',
+            dataIndex: 'name',
+            key: 'name',
+          },{
+            title: '工单总数',
+            dataIndex: 'total',
+            key: 'total',
+            width: 72,
+            render: (overdue) => (
+                <Tag color='blue' key={overdue}>
+                    {overdue}
+                </Tag>
+            )
+          },{
+            title: '未完成',
+            dataIndex: 'undone',
+            key: 'undone',
+            render: (overdue) => (
+                <Tag color='green' key={overdue}>
+                    {overdue}
+                </Tag>
+            )
+          },{
+            title: '逾期',
+            dataIndex: 'overdue',
+            key: 'overdue',
+            render: (overdue, record, index) => (
+                <Tag color='red' key={overdue} onClick = {() => {
+                  console.log(record, index)
+                  setTitle(record.name)
+                  setVisible(true)
+                }}>
+                  <div >
+                    {overdue}
+                  </div>
+                </Tag>
+            )
+          }
+    ]
+  return (
+    <div style={{padding: '0px 0px 50px 0px'}}>
+      <Radio.Group style={{padding: '4px'}} value={status} onChange={
+        (e) => {
+          setStatus(e.target.value)
+        }
+      }>
+        <Radio.Button value={statusR[0]}>今日</Radio.Button>
+        <Radio.Button value={statusR[1]}>全部</Radio.Button>
+      </Radio.Group>
+      <Table columns={columns} dataSource={cdata} pagination={false} size="small" rowKey="name"/>
+      <OrderModel title={title} status={status} visible={visible} pnum={1} onVisible={(val) =>{setVisible(val)}}/>
+    </div>
+  )
+}
